e433d31e70a6addf6274f8b02eba042bcec05b1f
Rubaiya2435 <164077424+Rubaiya2435@users.noreply.github.com>
2024-03-21 22:40:50 -0300

    my first commit

---
+++ app.ts
@@ -0,0 +1,30 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+//[]its an array {} we write our object or input in it
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operators to performs operation on calculator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+// console.log("Your value is " + answer.firstnumber + answer.secondnumber); //it not perform airthematic operation it concat them
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if(answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if(answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if(answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else{
+    console.log("Please select valid operator");
+}
